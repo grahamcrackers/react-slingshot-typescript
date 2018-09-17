daff58b1dcb612eedee8e27aef2528603d31731a
Graham Rogers <graham.rogers@stjude.org>
2018-09-17 16:36:36 -0500

    converted home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const HomePage = () => {
+const HomePage: React.SFC = () => {
   return (
     <div>
       <h1>React Slingshot</h1>
